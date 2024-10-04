46a81b8787a3bb0adaf87027a91b16579bd18262
JuanDiaz <juanben10xlr99@gmail.com>
2024-10-04 19:19:17 +0200

    Ajustar uso de .env para development y production

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,6 +13,8 @@ import router from "./routes/router";
 
 const app = express();
 
+let mongodbUri = process.env.MONGODB_URI as string;
+
 app.use(express.json());
 
 if (process.env.NODE_ENV === "development") {
@@ -23,22 +25,29 @@ if (process.env.NODE_ENV === "development") {
     }),
   );
 } else {
+  // Reemplaza localhost por mongodb en la URI de MongoDB
+  // para que la conexión se pueda realizar desde un contenedor
+  mongodbUri = mongodbUri.replace("localhost", "mongodb");
+
+  // Sirve los archivos estáticos de la carpeta dist (Frontend)
   app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 }
 
 app.use(express.urlencoded({ extended: true }));
 
 mongoose
-  .connect(process.env.MONGODB_URI as string)
+  .connect(mongodbUri)
   .then(() => console.log("Conectado a MongoDB"))
   .catch((error) => console.error("Error conectando a MongoDB:", error));
 
 app.use("/api/v1", router);
 
+//Respuesta para cualquier endpoint de la api no definido
 app.use("/api/*", (req, res) => {
   res.status(404).json({ message: "Endpoint no encontrado" });
 });
 
+//Redirige todas las rutas no definidas a la página de inicio
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "../../frontend/dist", "index.html"));
 });
