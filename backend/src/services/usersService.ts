afccc7523269b45eceddfeb93167402d90813e0f
JuanDiaz <juanben10xlr99@gmail.com>
2024-10-23 18:01:00 +0200

    Comments

--- backend/src/services/usersService.ts
+++ backend/src/services/usersService.ts
@@ -11,6 +11,8 @@ import prisma from "../libs/prisma";
 /**
  * Registers a new user in the database
  *
+ * Omit<User, "id" | "role">: userData -> register() -> Promise<User>
+ * 
  * @param userData - An object containing the user's data (email, password, name, etc.).
  * @returns {Promise<User>} - A promise that resolves with the newly created user.
  * @throws {Error} - Throws an error if the user cannot be created.
@@ -29,6 +31,8 @@ const register = async (userData: Omit<User, "id" | "role">): Promise<User> => {
 /**
  * Finds a user by email in the database
  *
+ * Text: email -> findUserByEmail() -> Promise<User | null>
+ * 
  * @param email - The email of the user to search for.
  * @returns {Promise<User | null>} - A promise that resolves with the found user or null if not found.
  */
