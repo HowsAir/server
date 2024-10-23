4eac790c7eb5febdff97ce2cf3932025022c2438
JuanDiaz <juanben10xlr99@gmail.com>
2024-10-24 00:37:42 +0200

    Separated Server and Frontend Succesfully

--- frontend/src/api/data.ts
+++
@@ -1,8 +0,0 @@
-export interface MeasurementData {
-  _id: string;
-  fecha: Date;
-  ppm: number;
-  temperatura: number;
-  latitud: number;
-  longitud: number;
-}
--- frontend/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="text-center p-6 bg-black text-white text-lg">
-      <p>© 2024 Freshair. Todos los derechos reservados.</p>
-    </footer>
-  );
-};
-
-export default Footer;
