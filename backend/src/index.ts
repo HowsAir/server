48b505d379dd1c1ecbcc4763e594dfd6933bfe66
JuanDiaz <juanben10xlr99@gmail.com>
2024-10-23 18:00:27 +0200

    HTTPS Working for Production

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,9 +5,26 @@
  */
 
 import app from "./app";
+import "dotenv/config";
+import path from "path";
+import https from 'https';
+import fs from 'fs';
 
 const PORT = 3000;
+const IP = "0.0.0.0"
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port: ${PORT}`);
-});
+if (process.env.NODE_ENV == "development") {
+  const options = {
+    key: fs.readFileSync(path.join(__dirname, '../server.key')),
+    cert: fs.readFileSync(path.join(__dirname, '../server.cert')),
+  };
+
+  https.createServer(options, app).listen(PORT, IP,() => {
+    console.log(`HTTPS Server running on port: ${PORT}`);
+  });
+}
+else {
+  app.listen(PORT, IP, () => {
+    console.log(`Server running on port: ${PORT}`);
+  });
+}
