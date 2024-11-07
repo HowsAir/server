48757167cdeab39fc4d2239f678df7cea67b9614
JuanDiaz <juanben10xlr99@gmail.com>
2024-11-07 16:14:21 +0000

    refactored password validator

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,11 +1,11 @@
 import { check, ValidationChain } from 'express-validator';
 
-export const passwordValidationRules = (
-    fieldName = 'password'
-): ValidationChain[] => [
+export const passwordValidationRules = (fieldName = 'password'): ValidationChain[] => [
     check(fieldName)
-        .isLength({ min: 8 })
-        .withMessage('Password must be at least 8 characters long')
+        .notEmpty()
+        .withMessage('Password is required')
+        .isLength({ min: 6 })
+        .withMessage('Password needs to be 6 characters long')
         .matches(/[A-Z]/)
         .withMessage('Password must contain at least one uppercase letter')
         .matches(/[a-z]/)
